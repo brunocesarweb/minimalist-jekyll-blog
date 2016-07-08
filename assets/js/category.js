6531a03391a98e179fc1e49fe190e4aad5785682
danilo tudisco <danilo.tudisco@viewit.com.br>
2016-07-08 18:17:52 -0300

    página interna de busca por categoria

---
+++ assets/js/category.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+   
+//   var valueCategoryList = []; 
+//   $("#categoryList > li").each(function(){
+//       valueCategoryList.push($(this).attr("class"));
+//   });
+//
+//    var duplicatesArray = valueCategoryList;
+//    var uniqueArray = duplicatesArray.filter(function(elem, pos) {
+//        return duplicatesArray.indexOf(elem) === pos;
+//    });
+//
+//    console.log("Unique - " + uniqueArray);
+//    for(var i = 0; i < uniqueArray.length;i++){
+//        $(this).addClass(uniqueArray[i]);
+//    };
+    
+}); //ready.function
